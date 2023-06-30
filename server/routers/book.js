9d139cac95776ceed71a0384a66c1a9979b9b550
Nastasia777 <kuznenastia@gmail.com>
2023-06-30 12:45:30 +0100

    changed routers

--- server/routers/book.js
+++ server/routers/book.js
@@ -5,9 +5,9 @@ const bookController = require("../controllers/book");
 const bookRouter = Router();
 
 bookRouter.get("/", bookController.index);
-countryRouter.get("/:id", bookController.show);
-countryRouter.post("/", bookController.create);
-countryRouter.delete("/:id", bookController.destroy);
-countryRouter.patch("/:id", bookController.update);
+bookRouter.get("/:id", bookController.show);
+bookRouter.post("/", bookController.create);
+bookRouter.delete("/:id", bookController.destroy);
+bookRouter.patch("/:id", bookController.update);
 
 module.exports = bookRouter;
