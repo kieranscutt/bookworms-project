3d4c1c25b988e8c0da35efca170b6c3670ab8442
kieranscutt <kieranscutt142@gmail.com>
2023-07-03 09:13:40 +0100

    Adding functions to contollers in book and user.js

--- server/controllers/book.js
+++ server/controllers/book.js
@@ -2,8 +2,27 @@ const Book = require("../models/Book")
 
 async function index (req, res) {
     try {
-        
+        const books = await Book.getAll();
+        res.json(books)
     } catch (err) {
         res.status(500).json({error: err.message})
     }
 }
+
+async function show (req, res) {
+    try {
+           const name = req.params.title
+           const book = await Book.getOneByName(name);
+           res.json(book)
+    } catch (err) {
+        res.status(404).json({error: err.message})
+    }
+}
+
+
+
+
+
+
+module.exports = { index, show }
+
