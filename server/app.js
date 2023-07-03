7717a7eeee512fc39413acb7a93c746a2aa235a9
Nastasia777 <kuznenastia@gmail.com>
2023-07-03 13:54:12 +0100

    Resolved merge conflict by incorporating both suggestions.

--- server/app.js
+++ server/app.js
@@ -1,10 +1,6 @@
 const express = require("express");
 const cors = require("cors");
 
-<<<<<<< HEAD
-=======
-
->>>>>>> d0715cfa89abe757930ca6814b61a9e794607516
 const logRoutes = require("./middleware/logger");
 const bookRouter = require("./routers/book");
 const userRouter = require("./routers/user");
@@ -26,7 +22,3 @@ app.use("/books", bookRouter);
 app.use("/users", userRouter);
 
 module.exports = app;
-<<<<<<< HEAD
-=======
-
->>>>>>> d0715cfa89abe757930ca6814b61a9e794607516
--- server/index.js
+++ server/index.js
@@ -1,16 +1,8 @@
-<<<<<<< HEAD
-=======
-
->>>>>>> d0715cfa89abe757930ca6814b61a9e794607516
 require("dotenv").config();
 
 const app = require("./app");
 const port = process.env.PORT;
 
 app.listen(port, () => {
-<<<<<<< HEAD
   console.log(`Server listening on port ${port}`);
 });
-=======
-  console.log(`Server listening on port ${port}`)});
->>>>>>> d0715cfa89abe757930ca6814b61a9e794607516
