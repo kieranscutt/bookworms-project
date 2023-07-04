5b47567e08e501ad206694bbc3a56294bec7820f
Nastasia777 <kuznenastia@gmail.com>
2023-07-04 09:15:48 +0100

    Updated db

--- server/database/setup.js
+++ server/database/setup.js
@@ -2,9 +2,10 @@ const fs = require("fs");
 require("dotenv").config();
 const db = require("./connect");
 
+// const path = require("path");
+
 const sql = fs.readFileSync("./database/setup.sql").toString();
 
 db.query(sql)
   .then((data) => console.log("Set-up complete."))
   .catch((error) => console.log(error));
-
