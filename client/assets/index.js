5f4d916539acfc70d9de9e58ffc58f2a8096c9c8
“harry_tb2112” <harry_tb@icloud.com>
2023-07-04 12:31:01 +0100

    can now browse by genre

--- client/assets/index.js
+++ client/assets/index.js
@@ -4,17 +4,17 @@ const genreButton = document.querySelector("#dropdown-button");
 const genreMenu = document.querySelector("#genre-menu");
 const userIcon = document.querySelector("#user-icon");
 const trendingContainer = document.querySelector(".trendingBooks");
-const trendingBook1 = document.querySelector("#trending1");
-const trendingBook2 = document.querySelector("#trending2");
-const trendingBook3 = document.querySelector("#trending3");
-const trendingBook4 = document.querySelector("#trending4");
-const trendingBook5 = document.querySelector("#trending5");
+const trendingBooks = document.querySelectorAll("#trendingBook");
+const genreBooks = document.querySelectorAll("#genreBook");
+const genreLinks = document.querySelectorAll("#genreLink");
+const genreGrid = document.querySelector(".genreGrid");
 
 hamburger.addEventListener("click", openBurger);
 genreButton.addEventListener("click", openGenre);
 userIcon.addEventListener("click", () => {
   window.location.assign("./user.html");
 });
+genreLinks.forEach((link) => link.addEventListener("click", loadGenreBooks));
 
 function openBurger() {
   burgerMenu.classList.toggle("open");
@@ -24,7 +24,7 @@ function openGenre() {
   genreMenu.classList.toggle("open");
 }
 
-async function loadTrendingBooks() {
+async function loadBooks() {
   const options = {
     headers: {
       Authorization: localStorage.getItem("token"),
@@ -34,19 +34,35 @@ async function loadTrendingBooks() {
   console.log(response);
   if (response.status === 200) {
     const books = await response.json();
-    trendingBook1.src = books[0].book_cover;
-    trendingBook1.alt = books[0].title;
-    trendingBook2.src = books[1].book_cover;
-    trendingBook2.alt = books[1].title;
-    trendingBook3.src = books[2].book_cover;
-    trendingBook3.alt = books[2].title;
-    trendingBook4.src = books[5].book_cover;
-    trendingBook4.alt = books[5].title;
-    trendingBook5.src = books[4].book_cover;
-    trendingBook5.alt = books[4].title;
+    books.forEach((book) => {
+      trendingBooks[books.indexOf(book)].src = book.book_cover;
+      trendingBooks[books.indexOf(book)].alt = book.title;
+    });
   } else {
     window.location.assign("./index.html");
   }
 }
 
-loadTrendingBooks();
+async function loadGenreBooks(event) {
+  genreGrid.classList.remove("hidden");
+  const link = event.target.innerHTML;
+
+  const options = {
+    headers: {
+      Authorization: localStorage.getItem("token"),
+    },
+  };
+  const response = await fetch("http://localhost:3000/books", options);
+  if (response.status === 200) {
+    const books = await response.json();
+    const filteredBooks = books.filter((book) => book.genre == link);
+    filteredBooks.forEach((book) => {
+      genreBooks[filteredBooks.indexOf(book)].src = book.book_cover;
+      genreBooks[filteredBooks.indexOf(book)].alt = book.title;
+    });
+  } else {
+    window.location.assign("./index.html");
+  }
+}
+
+loadBooks();
