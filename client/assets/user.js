8cbb347109e32a99a38bb31109473652324d0281
kieranscutt <kieranscutt142@gmail.com>
2023-07-04 16:37:34 +0100

    Merge branch 'development' of github.com:kieranscutt/bookworms-project into kieran

--- client/assets/user.js
+++ client/assets/user.js
@@ -1,1 +1,26 @@
-// user page
+const nameRef = document.querySelector(".name__input")
+const emailRef = document.querySelector(".email__input")
+const letterRef = document.querySelector(".profile__letter")
+
+async function emailInput() {
+    const response = await fetch("http://localhost:3000/users/email");
+    const data = await response.json();
+    emailRef.innerHTML = data.email
+}
+
+async function nameInput() {
+    const response = await fetch("https//:loacalhost:3000/users/first_name");
+    const data = await response.json();
+    nameRef.innerHTML = data.first_name
+}
+
+async function nameInput() {
+    const response = await fetch("https//:loacalhost:3000/users/first_name");
+    const data = await response.json();
+    nameRef.innerHTML = data.first_name[0]
+}
+
+
+letterInput()
+nameInput()
+emailInput()
