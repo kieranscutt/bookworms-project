41aa6145facacf9f562454a03eec979821c72298
“harry_tb2112” <harry_tb@icloud.com>
2023-07-07 10:37:37 +0100

    removed redundant routers

--- server/routers/user.js
+++ server/routers/user.js
@@ -6,8 +6,6 @@ const userRouter = Router();
 
 userRouter.post("/register", userController.register);
 userRouter.post("/login", userController.login);
-userRouter.delete("/:id", userController.destroy);
-userRouter.patch("/:id", userController.update);
 userRouter.get("/:id", userController.show);
 
 module.exports = userRouter;
